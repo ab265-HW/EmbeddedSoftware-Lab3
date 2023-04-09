6954f8c9bf4cc3b27767b215db4e0c7c48ec5dbc
unknown <ab265@hw.ac.uk>
2023-04-09 18:17:41 +0100

    added task 1 and 3

--- EmbSoft-lab3/EmbSoft-lab3.ino
+++ EmbSoft-lab3/EmbSoft-lab3.ino
@@ -37,7 +37,14 @@ void setup() {
   globalFreqResults.task3Desc.maxFreq = 1000;
 
    xTaskCreate(task2,   // pointer to the task function
-              "Task 1", // task name
+              "Task 2", // task name
+              1000,      // stack size in words
+              (void*)globalFreqResultsPtr,     // stack parameter
+              1,        // priority
+              NULL);    // task handle (not used)
+              
+   xTaskCreate(task3,   // pointer to the task function
+              "Task 3", // task name
               1000,      // stack size in words
               (void*)globalFreqResultsPtr,     // stack parameter
               1,        // priority
@@ -47,6 +54,24 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void task1(void *Ptr){
+  
+  TickType_t lastWake = xTaskGetTickCount();
+  const TickType_t period = 500/portTICK_PERIOD_MS;
+
+  for( ;; )
+    {
+      digitalWrite(Output_Pin_2, HIGH);
+      delayMicroseconds(200);
+      digitalWrite(Output_Pin_2,LOW);
+      delayMicroseconds(50);
+      digitalWrite(Output_Pin_2,HIGH);
+      delayMicroseconds(30);
+      digitalWrite(Output_Pin_2,LOW);
+      vTaskDelayUntil(&lastWake,period);
+    }
 }
 
 void task2(void *freqResultsPtr){
@@ -69,6 +94,26 @@ void task2(void *freqResultsPtr){
       Serial.println("Task2 Frequency is: " + String(globalFreqResults.task2Desc.measuredFreq));
       vTaskDelayUntil(&lastWake,period);
     }
+}
 
-  //vTaskDelete( NULL );
+void task3(void *freqResultsPtr){
+
+  TickType_t lastWake = xTaskGetTickCount();
+  const TickType_t period = 500/portTICK_PERIOD_MS;
+
+  for( ;; )
+    {
+      struct FreqResults *freqResPtr = (struct FreqResults *) freqResultsPtr;
+      //This is the line needing edited to add new tasks
+      struct FreqTaskDesc *taskDescPtr = &(freqResPtr->task3Desc);
+      
+      int waitTime = (int)((1000000.0/(taskDescPtr->minFreq))+10);
+      
+      bool currentState = digitalRead(0);
+      int halfWaveLengthMicro = pulseIn(0, !currentState, waitTime);
+      if (halfWaveLengthMicro <= 0) {(taskDescPtr->measuredFreq) = 0;}
+      else {int WaveLengthMicro = 2*halfWaveLengthMicro;(taskDescPtr->measuredFreq) = 1000000/WaveLengthMicro;}
+      Serial.println("Task3 Frequency is: " + String(globalFreqResults.task3Desc.measuredFreq));
+      vTaskDelayUntil(&lastWake,period);
+    }
 }
